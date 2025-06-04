ac26ca722d8f7fe063f9f386ee9a9a924017e093
Aksaya Venugopal <86884983+AksayaVenugopal@users.noreply.github.com>
2025-06-04 23:18:38 +0530

    Update openai12.py

--- auto_resume/openai12.py
+++ auto_resume/openai12.py
@@ -1,6 +1,6 @@
 import openai
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyB781IlY0ddAzwSYVVg6_GIwLqOkKl0Tj0")
+genai.configure(api_key="Secret key here")
 model = genai.GenerativeModel('gemini-pro')
 response = model.generate_content("what is fruits?")# Specify the model
 print(response)
